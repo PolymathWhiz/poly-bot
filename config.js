63faf8bb4977ca3d9d80d417f61937b6da62520e
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-02-02 13:53:25 +0000

    good. more to come

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: '5GYJ78nPfdt4J46Rf9ePBl6um',
+  consumer_secret: 'kaL8VbVeIOraVhkbzqSs2z3tahnF3BQSOMt0BYDcTdNmY0WJEO',
+  access_token_key: '3654240495-owh4DDN2LWMCJCiwbU5u9mn77ztwADRAxSQbsca',
+  access_token_secret: 'JDpbC8Jc2v5I6i8t8HJnBWCqFE5f34H0chJerBc5jmv46'
+}
